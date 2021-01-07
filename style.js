99753b32dfcb1497698e4181d6b92a19169f3b76
adrinv2 <55081904+adrinv2@users.noreply.github.com>
2021-01-07 10:57:42 +0100

    ..

---
+++ style.js
@@ -0,0 +1,18 @@
+function myFunction1() {
+  document.getElementById("landing").style.zIndex = "1";
+  document.getElementById("thesis").style.zIndex = "-1";
+  document.getElementById("credits").style.zIndex = "-2";
+}
+
+
+function myFunction2() {
+  document.getElementById("thesis").style.zIndex = "1";
+  document.getElementById("landing").style.zIndex = "-1";
+  document.getElementById("credits").style.zIndex = "-2";
+}
+
+function myFunction3() {
+  document.getElementById("credits").style.zIndex = "1";
+  document.getElementById("thesis").style.zIndex = "-1";
+  document.getElementById("landing").style.zIndex = "-2";
+}
