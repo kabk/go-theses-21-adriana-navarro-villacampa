85ff9f52a0e4d21c74aeb6059e5e7ac82febf727
adrinv2 <55081904+adrinv2@users.noreply.github.com>
2021-01-20 18:19:26 +0100

    ..

--- style.js
+++ style.js
@@ -1,9 +1,6 @@
-function PopUp(){
-    document.getElementById('ac-wrapper').style.display="none";
-};
 
 
-function myFunction() {
-  var popup = document.getElementByClass("myPopup");
-  popup.classList.toggle("show");
-};
+
+  function PopUp(){
+      document.getElementById('ac-wrapper').style.display="none";
+  };
