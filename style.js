fde59c4fd396fbc1f46ba24218aa859673a8f647
adrinv2 <55081904+adrinv2@users.noreply.github.com>
2021-01-20 11:21:19 +0100

    ..

--- style.js
+++ style.js
@@ -1,21 +1,21 @@
-function myFunction1() {
-  document.getElementById("landing").style.zIndex = "3";
-  document.getElementById("thesis").style.zIndex = "-1";
-  document.getElementById("credits").style.zIndex = "-2";
-}
-
-
-function myFunction2() {
-  document.getElementById("thesis").style.zIndex = "1";
-  document.getElementById("landing").style.zIndex = "-1";
-  document.getElementById("credits").style.zIndex = "-2";
-}
-
-function myFunction3() {
-  document.getElementById("credits").style.zIndex = "1";
-  document.getElementById("thesis").style.zIndex = "-1";
-  document.getElementById("landing").style.zIndex = "-2";
-};
+// function myFunction1() {
+//   document.getElementById("landing").style.zIndex = "3";
+//   document.getElementById("thesis").style.zIndex = "-1";
+//   document.getElementById("credits").style.zIndex = "-2";
+// }
+//
+//
+// function myFunction2() {
+//   document.getElementById("thesis").style.zIndex = "1";
+//   document.getElementById("landing").style.zIndex = "-1";
+//   document.getElementById("credits").style.zIndex = "-2";
+// }
+//
+// function myFunction3() {
+//   document.getElementById("credits").style.zIndex = "1";
+//   document.getElementById("thesis").style.zIndex = "-1";
+//   document.getElementById("landing").style.zIndex = "-2";
+// };
 
 
 
