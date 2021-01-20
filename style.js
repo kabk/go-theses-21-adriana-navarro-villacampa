f4219f8eb3e16bdc479e3abebd7e5091c6389960
adrinv2 <55081904+adrinv2@users.noreply.github.com>
2021-01-20 17:23:50 +0100

    ..

--- style.js
+++ style.js
@@ -1,31 +1,9 @@
-// function myFunction1() {
-//   document.getElementById("landing").style.zIndex = "3";
-//   document.getElementById("thesis").style.zIndex = "-1";
-//   document.getElementById("credits").style.zIndex = "-2";
-// }
-//
-//
-// function myFunction2() {
-//   document.getElementById("thesis").style.zIndex = "1";
-//   document.getElementById("landing").style.zIndex = "-1";
-//   document.getElementById("credits").style.zIndex = "-2";
-// }
-//
-// function myFunction3() {
-//   document.getElementById("credits").style.zIndex = "1";
-//   document.getElementById("thesis").style.zIndex = "-1";
-//   document.getElementById("landing").style.zIndex = "-2";
-// };
-
-
-
-
 function PopUp(){
     document.getElementById('ac-wrapper').style.display="none";
 };
 
 
 function myFunction() {
-  var popup = document.getElementById("myPopup");
+  var popup = document.event.target;
   popup.classList.toggle("show");
 };
